405da77d4f32903aed6940d3ee07a3ba27a226f1
valeria0610 <valeriacopete.0610@gmail.com>
2020-09-04 15:31:53 -0500

    Adicionada la clase isDone when is checkout

--- src/main.js
+++ src/main.js
@@ -9,8 +9,18 @@ addNoteBtn.onclick = () => {
     let checkbox = document.createElement('input');
 
     checkbox.type = "checkbox";
+    //Para tachar la tarea realizada
+    checkbox.onclick = function () {
+        let parent = this.parentElement;
+        if (this.checked) {
+            parent.classList.add('isDone');
+        } else {
+            parent.classList.remove('isDone');
+        }
+    }
+
     note.classList.add('note');
-    note.innerHTML = newNoteInput.value;
+    note.innerHTML = `<div>${newNoteInput.value}</div>`;
     note.appendChild(checkbox);
     newNoteInput.value = '';
     notes.appendChild(note);
